a84a275ed7db823272df6418a21cd43899c8d95e
ninamoutonnet <nm2318@ic.ac.uk>
2020-11-29 11:50:52 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Main {
+    public static void main(String[] args){
+
+    }
+    public void MakeGetRequest() {
+        try {
+            URL myURL = new URL("http://imperial.ac.uk");
+            HttpURLConnection conn = (HttpURLConnection) myURL.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "text/html");
+            conn.setRequestProperty("charset", "utf-8");
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(myURL.openStream()));
+
+            String inputLine;
+            // Read the body of the response
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println(inputLine);
+            }
+            in.close();
+        } catch (Exception e) {
+            System.out.println("Something is wrong with URL");
+        }
+    }
+}
