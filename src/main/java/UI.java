92d576667078d9b07ae55f81eabac086e8d31460
louisebaron <louisebaron2000@hotmail.com>
2020-12-11 19:07:02 +0000

    adding medicine type & changing the layout

--- src/main/java/UI.java
+++ src/main/java/UI.java
@@ -19,11 +19,73 @@ public class UI extends JPanel{
         Color mycol = new Color(180, 200, 100); // Color
         mainPanel.setBackground(mycol);
 
+        JPanel myPanel = new JPanel();
+
+        Vector<String> cat = new Vector<String>();
+        cat.addElement("Select a medicine type");
+        cat.addElement("Cold and flu");
+        cat.addElement("Skincare");
+        cat.addElement("Headaches and pain relief");
+        cat.addElement("Digestion");
+        cat.addElement("Allergy");
+        cat.addElement("First aid");
+        final JComboBox<String> categories = new JComboBox<String>(cat);
+        categories.setVisible(true);
+        String var = (String)categories.getSelectedItem();
+        //problem with this is that the value is the initial one, ie always 1
+        System.out.println("the selected type of medication is: " + var);
+        mainPanel.add(categories);
+
+        //use this combo box to see the list of possible medicine to select,
+        //ideally it would go through the heroku db and select all the names of the available medicine
+        Vector<String> medname = new Vector<String>();
+        medname.addElement("Select a medicine brand");
+        medname.addElement("paracetamol");
+        medname.addElement("doliprane");
+        medname.addElement("spasphon");
+        medname.addElement("smecta");
+        final JComboBox<String> med = new JComboBox<String>(medname);
+        med.setVisible(true);
+        mainPanel.add(med);
+
+        //set the combo box
+
+        Vector<String> choices = new Vector<String>();
+        choices.addElement("Select amount");
+        choices.addElement("1");
+        choices.addElement("2");
+        choices.addElement("3");
+        choices.addElement("4");
+        final JComboBox<String> cb = new JComboBox<String>(choices);
+        cb.setVisible(true);
+        String varName = (String)cb.getSelectedItem();
+        //problem with this is that the value is the initial one, ie always 1
+        System.out.println("the selected number of medication is: " + varName);
+        mainPanel.add(cb);
+
+        //set the combo box
+        //when using the servlet, we have to go through the db and names
+        Vector<String> name = new Vector<String>();
+        name.addElement("Select a customer");
+        name.addElement("Jon Smith");
+        name.addElement("Louise Baron");
+        name.addElement("Nina Moutonnet");
+        name.addElement("Irene Mangialomini");
+        final JComboBox<String> names = new JComboBox<String>(name);
+        names.setVisible(true);
+        mainPanel.add(names);
+
+        //set the date
+        Label label1 = new Label("Name:");
+        TextField textField = new TextField ();
+        // mainPanel.add(textField); //not adding it for now
+
+
         // stock button
         View_stock = new JButton("View Stock");
         mainPanel.add(View_stock);
 
-       // med handling button
+        // med handling button
         add_remove_med = new JButton("Add or remove a medicine");
         mainPanel.add(add_remove_med);
         add_remove_med.addActionListener(new ActionListener() {
@@ -51,47 +113,6 @@ public class UI extends JPanel{
 
         updateStock = new JButton("Update Stock");
         mainPanel.add(updateStock);
-
-        //use this combo box to see the list of possible medicine to select,
-        //ideally it would go through the heroku db and select all the names of the available medicine
-        Vector<String> medname = new Vector<String>();
-        medname.addElement("paracetamol");
-        medname.addElement("doliprame");
-        medname.addElement("spasphon");
-        medname.addElement("smecta");
-        final JComboBox<String> med = new JComboBox<String>(medname);
-        med.setVisible(true);
-        mainPanel.add(med);
-
-        //set the combo box
-
-        Vector<String> choices = new Vector<String>();
-        choices.addElement("1");
-        choices.addElement("2");
-        choices.addElement("3");
-        choices.addElement("4");
-        final JComboBox<String> cb = new JComboBox<String>(choices);
-        cb.setVisible(true);
-        String varName = (String)cb.getSelectedItem();
-        //problem with this is that the value is the initial one, ie always 1
-        System.out.println("the selected number of medication is: " + varName);
-        mainPanel.add(cb);
-
-        //set the combo box
-        //when using the servlet, we have to go through the db and names
-        Vector<String> name = new Vector<String>();
-        name.addElement("Jon Smith");
-        name.addElement("Louise Baron");
-        name.addElement("Nina Moutonnet");
-        name.addElement("Irene Mangialomini");
-        final JComboBox<String> names = new JComboBox<String>(name);
-        names.setVisible(true);
-        mainPanel.add(names);
-
-        //set the date
-        Label label1 = new Label("Name:");
-        TextField textField = new TextField ();
-        // mainPanel.add(textField); //not adding it for now
 
     }
 
