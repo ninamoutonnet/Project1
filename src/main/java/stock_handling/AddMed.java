3438ef7dad644a538ce6b9f518cf27b04b2a4eb6
ninamoutonnet <nm2318@ic.ac.uk>
2021-01-08 19:36:10 +0000

    Made the add medicine function was better, still need to figure out how to send info to the db, it seems that it does not work because i want to send booleans, doubles, ints and strings to the db and my sql commands must be wrong

--- src/main/java/stock_handling/AddMed.java
+++ src/main/java/stock_handling/AddMed.java
@@ -1,6 +1,7 @@
 package stock_handling;
 
 import GUI.SortedComboBoxModel;
+import db_handling.GetDB_medicine;
 
 import javax.swing.*;
 import java.awt.*;
@@ -10,9 +11,11 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 
 
 public class AddMed {
+    GetDB_medicine info = new GetDB_medicine();
 
     public AddMed() {
         JFrame fr = new JFrame();  //dummy JFrame to ensure that the window popping up is on top of anything else
@@ -22,21 +25,18 @@ public class AddMed {
 
         JTextField amount = new JTextField(13);
         JTextField name = new JTextField(13);
-        JTextField brand = new JTextField(13);
         JTextField qty = new JTextField(13);
         JTextField sprice = new JTextField(3);
         JTextField pprice = new JTextField(6);
         JTextField fullstock = new JTextField(6);
-        String limitation;
-        String CAT;
+        String LIM = " ";
+        String CAT = " ";
+        String DES = " ";
 
         JPanel myPanel = new JPanel();
-        myPanel.setLayout(new GridLayout(9, 2));
-        myPanel.add(new JLabel("Medicine name/description:"));
+        myPanel.setLayout(new GridLayout(10, 2));
+        myPanel.add(new JLabel("Medicine name/brand:"));
         myPanel.add(name);
-        myPanel.add(Box.createHorizontalStrut(15)); // a spacer
-        myPanel.add(new JLabel("Medicine brand:"));
-        myPanel.add(brand);
         myPanel.add(Box.createHorizontalStrut(15)); // a spacer
         myPanel.add(new JLabel("Medicine amount per box/bottle:"));
         myPanel.add(amount);
@@ -61,80 +61,103 @@ public class AddMed {
         limited.setVisible(true);
         myPanel.add(limited);
 
-        limitation = limited.getSelectedItem().toString(); // this does not work --> always returns 'Yes' even though 'No' is selected
-        System.out.println(limitation);
 
-        /* No need for that as name is the same as description
+        //CATEGORY
         myPanel.add(Box.createHorizontalStrut(15)); // a spacer
-        myPanel.add(new JLabel("Description "));
-
-        //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-        // WE SHOULD BE ABLE TO SELECT THE CHOICE 2 FROM THE DB ONLINE!
-
-        //
-        String[] choices2 = {"Relief","OTHER","Tabs","Antiseptic","Hand Sanitizer","Plasters","Liquid","First Defense","Night Nurse","Max","Standard","Day and Night","Mucus relief","4 flu","Vaporub","Moisturising cream","Exfoliating cleanser","Skin cream","Skin relief cream","Face Scrub","Psiorasis cream","Repair and Restore","Eczema cream","Meltlets","Express","Max Strengh","Headache","Extra","Triple action","Original","Soluble","Blackcurrant","Lemon","Chewable","Advance"};
+        myPanel.add(new JLabel("Category "));
+        ArrayList<String> str1 = info.getCategory(); //GET FROM DB
+        String[] choicesCategory = new String[str1.size()];
+        for (int i = 0; i < str1.size(); i++){
+            choicesCategory[i] = str1.get(i);
+        }
         //this class of combo box makes everything sorted alphabetically.
-        GUI.SortedComboBoxModel<String> model = new GUI.SortedComboBoxModel<String>(choices2);
+        SortedComboBoxModel<String> modelCategory = new SortedComboBoxModel<String>(choicesCategory);
+        JComboBox<String> comboBoxCategory = new JComboBox<String>( modelCategory );
+        comboBoxCategory.setVisible(true);
+        myPanel.add(comboBoxCategory);
+
+
+        //DESCRIPTION
+        myPanel.add(Box.createHorizontalStrut(15)); // a spacer
+        myPanel.add(new JLabel("Description:"));
+        ArrayList<String> str = info.getDescription(); //GET FROM DB
+        String[] choicesDescription = new String[str.size()];
+        for (int i = 0; i < str.size(); i++){
+            choicesDescription[i] = str.get(i);
+        }
+        //this class of combo box makes everything sorted alphabetically.
+        SortedComboBoxModel<String> model = new SortedComboBoxModel<String>(choicesDescription);
         JComboBox<String> comboBox = new JComboBox<String>( model );
         comboBox.setVisible(true);
         myPanel.add(comboBox);
-         */
 
-        myPanel.add(Box.createHorizontalStrut(15)); // a spacer
-        myPanel.add(new JLabel("Category "));
-
-        /*
-
-        HERE WE WILL TAKE THE DB FROM HEROKU, USE ALL THE DESCRIPTION TO PUT THEM IN AN ARRAY OF STRING.
-        THIS WILL THEN BE FED INTO A SORTING ALGORITHM AND USED FOR THE DROP DOWN MENU
-        RIGHT NOW I JUST TYPED THE NAMES MYSELF
-
-         */
-
-        // Need to extract a string from this
-        String[] choices3 = {"Allergy","OTHER","First Aid","Cold and flu","Skincare","Headache and pain relief","Digestion"};
-        SortedComboBoxModel<String> model2 = new SortedComboBoxModel<String>(choices3);
-        JComboBox<String> comboBox2 = new JComboBox<String>(model2);
-        comboBox2.setVisible(true);
-        myPanel.add(comboBox2);
-
-        CAT = comboBox2.getSelectedItem().toString(); // this does not work --> always returns 'Allergy' even though another one is chosen
-        System.out.println(CAT);
 
         int result = JOptionPane.showConfirmDialog(fr, myPanel,
                 "Please Enter the medicine details", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
         if (result == JOptionPane.OK_OPTION) {
-            // Get the inputs from the person using the app --> information for one client
-            String B = brand.getText();
-            String A = amount.getText();
-            String CS = qty.getText();
-            String SP = sprice.getText();
-            String PP = pprice.getText();
-            String FS = fullstock.getText();
-            String N = name.getText();
-            String LIM;
+            //get the values of combobox once OK is pressed
+            LIM = limited.getSelectedItem().toString(); // this does not work --> always returns 'Yes' even though 'No' is selected
+            CAT = comboBoxCategory.getSelectedItem().toString(); // this does not work --> always returns 'Allergy' even though another one is chosen
+            DES = comboBox.getSelectedItem().toString();
 
-            if (limitation == "Yes")
-                LIM = "TRUE";
-            else
-                LIM = "FALSE";
+
+
+            // Get the inputs from the person using the app --> information for one client
+            String A = amount.getText();
+           // String CS = qty.getText();
+            Integer CS=0;
+            try{
+                CS = Integer.parseInt(qty.getText());
+            }catch (Exception e){
+                System.out.println("Not an current stock integer ");
+            }
+
+           // String SP = sprice.getText();
+            Double SP =0.0;
+            try{
+                SP = Double.parseDouble(sprice.getText());
+            }catch (Exception e){
+                System.out.println("Not a double sales price");
+            }
+            //String PP = pprice.getText();
+            Double PP=0.0;
+            try{
+                PP = Double.parseDouble(pprice.getText());
+            }catch (Exception e){
+                System.out.println("Not a double pruchase price");
+            }
+            //String FS = fullstock.getText();
+            Integer FS=0;
+            try{
+                FS = Integer.parseInt(fullstock.getText());
+            }catch (Exception e){
+                System.out.println("Not an integer fullstock ");
+            }
+
+            String N = name.getText();
+
+            boolean LIM2=true;
+            if(LIM.equalsIgnoreCase("yes")){
+                LIM2 = true;
+            }else if(LIM.equalsIgnoreCase("no")){
+                LIM2=false;
+            }
+            /*System.out.println(A);
+            System.out.println(LIM);
+            System.out.println(CAT);
+            System.out.println(DES);
+            System.out.println(CS);
+            System.out.println(SP);
+            System.out.println(PP);
+            System.out.println(FS);
+            System.out.println(N);*/
+
+
 
             // Include this new product in the DB on Heroku (see ProjectServlet for more details)
-            makePostRequest(B, A, SP, PP, FS, LIM, N, CAT, CS); // make sure that the client is not added if he/she is already in the DB - need to find a solution for that
+            makePostRequest(N, A, SP, PP, FS, LIM2, DES, CAT, CS); //check that it does not already exist
 
             fr.dispose(); //discard of the dummy jframe
-
-            // for now -> print text but later use this loop to get data
-            /*  System.out.println("Medicine brand: " + brand.getText());
-            System.out.println("Medicine name: " + name.getText());
-            System.out.println("Quantity: " + name.getText());
-            System.out.println("Sale price: " + sprice.getText());
-            System.out.println("Purchase price : " + pprice.getText());
-            System.out.println("Full stock is : " + fullstock.getText());
-            System.out.println("Limitation : " + limited.getSelectedItem());
-            System.out.println("Description : " + comboBox.getSelectedItem());
-            System.out.println("Category : " + comboBox2.getSelectedItem());*/
 
             /*
            if(comboBox.getSelectedItem()=="OTHER") { //add a new element to the description
@@ -164,12 +187,12 @@ public class AddMed {
         }
     }
 
-    public static void makePostRequest(String B, String A, String SP, String PP, String FS, String LIM, String N, String CAT, String CS) {
-        try {
-            // Convert data types to String
 
+    // Method making a POST request to the servlet to add a new client to the DB - check that POST is the correct method (could be PUT)
+    public static void makePostRequest(String N, String A, Double SP, Double PP, Integer FS, Boolean LIM, String DES, String CAT, Integer CS) {
+        try {
             // This is the SQL query included in the body of the POST request = instructions
-            String message = "INSERT INTO products (brand,amount,\"sprice \",pprice,\"fullstock \",\"limitation \",\"description \",\"category \",currentstock) values( '"+B+"','"+A+"',"+SP+","+PP+","+FS+","+LIM+",'"+N+"','"+CAT+"',"+CS+");";
+            String message = "INSERT INTO products (brand,amount,sprice,pprice,fullstock,limitation,description,category,currentstock) values( '"+N+"','"+A+"',"+SP+","+PP+","+FS+",'"+LIM+"','"+DES+"','"+CAT+"',"+CS+");";
             byte[] body = message.getBytes(StandardCharsets.UTF_8);
 
             // The URL maps to the servlet (check that this is the correct way to say it)
@@ -202,5 +225,8 @@ public class AddMed {
             System.out.println("Something went wrong");
         }
     }
+
+
+
 }
 
